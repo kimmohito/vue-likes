945e9d89562e8f39840bbaa1aba36cde25ef2f9d
Kim Mohito <kimmohito@gmail.com>
2022-09-30 13:38:08 +0800

    update

--- src/components/myLikes.vue
+++ src/components/myLikes.vue
@@ -1,12 +1,9 @@
 <template>
-
     <p>Click on the likes button below:</p><br>
-
     <button v-on:click="like()">
         <span v-if="likes >0">{{likes}}</span>
         Like<span v-if="likes > 1 ">s</span> 👍</button>
 </template>
-]
 
 <script>
 export default {
